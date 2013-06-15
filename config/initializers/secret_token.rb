c99544260b54c7cddba1d1fc8df31e3e97148d3f
Andrew Watts <ahwatts@gmail.com>
2013-06-15 13:32:55 -0400

    Initial commit of a default Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Songviz::Application.config.secret_token = 'fdb6583bb6dafcbe711d04649cb728c2a508af4eab3da42355aee73981a1994fb071591492c8ef4a1a9d63eb75cd8157293760446d5bc11185e5ae90ecbf222d'
