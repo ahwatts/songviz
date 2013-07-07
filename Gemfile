6892a6dd2404309385e2cb7ebf38e3e3c660ecdd
Andrew Watts <ahwatts@gmail.com>
2013-07-07 16:40:49 -0400

    Upgrade to Rails 4.0 and Ruby 2.0.

--- Gemfile
+++ Gemfile
@@ -1,21 +1,22 @@
 source 'https://rubygems.org'
 
+gem 'coffee-rails', '~> 4.0.0'
 gem 'haml'
 gem 'jquery-rails'
-gem 'rails', '~> 3.2.13'
+gem 'jbuilder', '~> 1.2'
+gem 'rails', '~> 4.0.0'
+gem 'sass-rails', '~> 4.0.0'
 gem 'sqlite3'
-
-group :assets do
-  gem 'coffee-rails', '~> 3.2.1'
-  gem 'sass-rails', '~> 3.2.3'
-  gem 'uglifier', '>= 1.0.3'
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
-end
+gem 'thin'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
 
 group :development do
   gem 'git-up'
   gem 'pry'
   gem 'pry-rails'
-  gem 'thin'
+end
+
+group :doc do
+  gem 'sdoc', require: false
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Songviz::Application.config.secret_token = 'fdb6583bb6dafcbe711d04649cb728c2a508af4eab3da42355aee73981a1994fb071591492c8ef4a1a9d63eb75cd8157293760446d5bc11185e5ae90ecbf222d'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Songviz::Application.config.secret_key_base = '65f221532d437cd6b1379a8816c6e37555c5423f308b3bcbcec9f8868555224c74911843fc4d044bfdf10371614e21021ee77666ba41ca555580224fc04d2aa5'
